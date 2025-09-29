6eddc73456499e6742778634c7530e74f5b908e6
QwertyVole <fakeqwertzy@gmail.com>
2025-09-29 16:49:24 +0200

    the begining

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import brian.motors as motors
+import brian.sensors as sensors
+import time
+
+rm = motors.NXTMotor(motors.MotorPort.A)
+lm = motors.NXTMotor(motors.MotorPort.B)
+ls =sensors.NXT.LightSensorNXT(sensors.SensorPort.S1)
+speed = 100
+
+
+while True:
+    print("CW[A]...")
+    rm.wait_until_ready()
+    lm.wait_until_ready()
+    rm.run_at_speed(speed)
+    lm.run_at_speed(speed)
+
+    print("Wait...")
+    time.sleep(1)
